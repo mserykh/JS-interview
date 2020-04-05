846b39a6b7dcfa64707006055cefff287611aac9
Marie Serykh <marie.serykh@gmail.com>
2020-04-05 17:21:30 +0200

    Created an input for a question * Created objects * Added new properties for `box`

--- 22-obj-prop-2.js
+++ 22-obj-prop-2.js
@@ -1,3 +1,17 @@
 /** *
  * What will be the output from the console.log and why?
  */
+let box = {};
+let bag = {
+    prop: "bag",
+    can: "box"
+};
+
+let can = {
+    prop: "can",
+    bag: "box"
+};
+box[bag] = "Monday";
+box[can]= "Tuesday";
+
+console.log(box[bag]);
