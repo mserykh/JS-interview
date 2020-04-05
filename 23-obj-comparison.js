ff690bf53cf231a3578a96a5a73985ac980e04d8
Marie Serykh <marie.serykh@gmail.com>
2020-04-05 17:38:03 +0200

    Created 2 objects and compared them

---
+++ 23-obj-comparison.js
@@ -0,0 +1,21 @@
+/***
+ * What will be the output of this code and why?
+ */
+
+ let obj = { a: 1};
+ let objb = { a: 1 };
+
+ if ({ a: 1} === { a: 1}) {
+     console.log("Monday");
+ } else if ({ a: 1} == { a: 1}) {
+    console.log("Tuesday");
+ } else if (obj == { a: 1}) {
+    console.log("Wednesday");
+ } else if (obj == objb) {
+    console.log("Thursday");
+ } else if (Object.is(obj, objb)) {
+    console.log("Friday");
+ } else {
+    console.log("Saturday");
+ }
+ 
