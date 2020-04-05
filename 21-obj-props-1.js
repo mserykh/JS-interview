7ef1e836ffb8714dea4c10cee8ffe6cc68e3c4b8
Marie Serykh <marie.serykh@gmail.com>
2020-04-05 17:00:20 +0200

    Created an object `obj `

--- 21-obj-props-1.js
+++ 21-obj-props-1.js
@@ -2,3 +2,16 @@
 /**
  * What will be the output of the following console.log?
  */
+
+ let obj = {
+     'a': 'three',
+     b: 4,
+     'c': "five",
+     a: 3,
+     'b': "four",
+     "c": 'fem',
+     "a": "tre",
+     "b": 'fyra',
+     c: 5
+ };
+ console.log(obj);
